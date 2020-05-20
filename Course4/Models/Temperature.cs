ac21d61044b4adf74e8826301620396a38f769ba
giovanni <giovanniayayi.dalmeida@gmail.com>
2020-05-20 16:06:49 -0700

    Add project files.

---
+++ Course4/Models/Temperature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course4.Models
+{
+    public class Temperature
+    {
+        public int Grandeur { get; set; };
+    }
+}
